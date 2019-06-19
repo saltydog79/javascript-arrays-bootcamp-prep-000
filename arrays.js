76125f561f809e36430b39c494e487d76bea6169
Emily <aspentree79@gmail.com>
2019-06-19 14:38:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,7 +33,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 
 function removeElementFromBeginningOfArray(array) {
 var arrayTwo = array;
-  array.slice(1);
+  arrayTwo.slice(1);
   return arrayTwo
 }
 
